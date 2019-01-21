85d9ba5d94c5089b346226a4323ece8fd130b996
Daniel Jarc <daniel.jarc@outlook.com>
2019-01-21 11:59:59 -0500

    Updated servo centering code and added project plan.

--- code/servo_centering/servo_centering.ino
+++ code/servo_centering/servo_centering.ino
@@ -1,12 +1,21 @@
+/*
+ * Description: Servo centering code (no potentiometer)
+ * Usage: Manually check servo min/max values by altering servo, servo_min, servo_max and deg values then uploading
+ * and checking for movement near min max positions (0 and 180). When there is no movement servo is at min or max. 
+ * Store/copy value for that servo for later use. 
+ */
+
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// Min and max values for individual servos (150 to 600 default) 
-const int servo_min[12] = { 120, 110, 120, 185, 175, 110, 120, 110, 110, 120, 155, 163 };
-const int servo_max[12] = { 600, 570, 590, 585, 600, 600, 600, 570, 580, 570, 570, 570 };
-const int servo = 11;
+// Min and max values for individual servos (150 to 600 default). Values are different for every servo. 
+// Change values until servo reaches mechanical min and max during tests... 
+const int servo_min[12] = { 120, 115, 120, 185, 175, 110, 120, 110, 110, 120, 155, 163 };
+const int servo_max[12] = { 600, 590, 590, 585, 600, 600, 600, 570, 580, 570, 570, 570 };
+
+int servo;
 int deg;
-int pulse_width;
+int pulse_width;  
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -14,34 +23,23 @@ void setup() {
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(60);
-  delay(10);
 }
 
-
+/*
+ * This will set servo 0 to 0 degrees based on the servo_min[0] value above,
+ * Wait 2 seconds, then set servo 0 to 180 degrees based on the servo_max[0] value.
+ */
 void loop() {
+  
+    servo = 0;
 
-    // Move to 0, 90, 180 degree positions using map() and servo min/max values...
-    Serial.print("Servo: ");
-    Serial.println(servo);
-    
-    deg = 0;
+    deg = 0; 
     pulse_width = map(deg, 0, 180, servo_min[servo], servo_max[servo]);
     pwm.setPWM(servo, 0, pulse_width);
-    Serial.print("0 degrees - pulse_width: ");
-    Serial.println(pulse_width);
-    delay(5000);
-    
-    deg = 90;
-    pulse_width = map(deg, 0, 180, servo_min[servo], servo_max[servo]);
-    pwm.setPWM(servo, 0, pulse_width);
-    Serial.print("90 degrees - pulse_width: ");
-    Serial.println(pulse_width);
-    delay(2000);    
-    
+    delay(2000);
+
     deg = 180;
     pulse_width = map(deg, 0, 180, servo_min[servo], servo_max[servo]);
     pwm.setPWM(servo, 0, pulse_width);
-    Serial.print("180 degrees - pulse_width: ");
-    Serial.println(pulse_width);
     delay(2000);
 }
