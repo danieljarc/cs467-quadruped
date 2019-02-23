4c64949a081833acf7cbdc0cfd080260e4887d5c
Michael <MikeNIke@users.noreply.github.com>
2019-02-23 11:42:08 -0800

    Updated Qt files

--- GUI_source_code/fangcontrol.h
+++ GUI_source_code/fangcontrol.h
@@ -4,6 +4,9 @@
 #include <QSerialPort>
 #include <QSerialPortInfo>
 #include <QDebug>
+#include <QStateMachine>
+#include <QEventTransition>
+#include <QPropertyAnimation>
 //#include <QtCore>
 #include <QtGui>
 #include <QtBluetooth>
@@ -13,7 +16,8 @@
 #include <QMessageBox>
 #include <QButtonGroup>
 //#include <QBluetoothDeviceDiscoveryAgent>
-//#include <QBluetoothSocket>
+#include <QBluetoothSocket>
+#include <QListWidget>
 
 namespace Ui {
 class FangControl;
@@ -36,7 +40,7 @@ private slots:
 
     void on_up_button_pressed();
 
-    void on_connect_button_clicked();
+    void on_connect_button_clicked(bool connect_enable);
 
     void on_mode_button_clicked();
 
@@ -50,27 +54,55 @@ private slots:
 
     void on_stand_button_pressed();
 
+    void on_up_button_released();
 
-    //void on_listWidget_itemClicked(QListWidgetItem *item);
+    void on_left_button_released();
+
+    void on_right_button_released();
+
+    void on_down_button_released();
+
+    void on_l1_button_released();
+
+    void on_r1_button_released();
 
     //void deviceDiscovered(const QBluetoothDeviceInfo &device);
 
+    //void on_devicesList_itemClicked(QListWidgetItem *item);
+
+    void connected();
+
+    void onSocketErrorOccurred(QBluetoothSocket::SocketError);
+
+    void readSocket();
+
+signals:
+    void connected(const QString &name);
+    void disconnected();
+    void socketErrorOccurred(const QString &errorString);
+    void messageReceived(const QString &sender, const QString &message);
+
 private:
-    enum State {NO_CONNECTION, CONNECTED, ST_MODE, SR_MODE, WG1_MODE, WG2_MODE};
-    State current_state;
+    QStateMachine *state_m;
+    QState *NO_CONNECTION;
+    QState *CONNECTED;
+    QState *ST_MODE;
+    QState *SR_MODE;
+    QState *WG1_MODE;
+    QState *WG2_MODE;
+
     Ui::FangControl *ui;
     QSerialPort *arduino;
     QString arduino_port_name;
+    bool connect_enable;
     static const quint16 arduino_uno_vendor_id;
     static const quint16 arduino_uno_product_id;
     bool arduino_available;
 
-    //QBluetoothDeviceDiscoveryAgent *agent = new QBluetoothDeviceDiscoveryAgent;
-    //QBluetoothSocket *socket;
-    QString btAddr;
+    //QBluetoothDeviceDiscoveryAgent *discoveryAgent = new QBluetoothDeviceDiscoveryAgent; // Used to search for bluetooth devices
+    QBluetoothSocket *socket = nullptr; // Sets up RFCOMM socket similar to TCP socket
+    QString btAddr = "98:D3:11:FC:1B:B0"; // Hexidecimal address of bluetooth device
 
-//    QButtonGroup* walk_group;
-//    QButtonGroup* stationary_group;
 };
 
 #endif // FANGCONTROL_H
